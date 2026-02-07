28d9089a2b1ae88612c6317acd61c187d10a3a4e
Jalil Andewadi <jalilandewadi027@gmail.com>
2026-02-07 13:47:05 +0530

    feat: update import paths to include file extensions and add new dependencies in package.json

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore.js";
+import { useThemeStore } from "../store/useThemeStore.js";
 import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const { theme } = useThemeStore();
   const [selectedImg, setSelectedImg] = useState(null);
 
   const handleImageUpload = async (e) => {
@@ -22,7 +24,7 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="h-screen pt-20">
+    <div className={`h-screen pt-20 ${theme}`}>
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
